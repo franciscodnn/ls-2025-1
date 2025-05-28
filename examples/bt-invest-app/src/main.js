97cb5dfbd8fc6b1a5db90a3cbb5df40355172f48
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2025-05-28 21:18:43 +0000

    Build Tools

---
+++ examples/bt-invest-app/src/main.js
@@ -0,0 +1,21 @@
+import { investments } from './data.js';
+import { InvestmentCard } from './components/InvestmentCard.js';
+
+// console.log(investments[0]);
+
+function addInvestment(investment) {
+    // 1. obter div com grid dos investments
+    const gridInvestments = document.querySelector('.investments')
+
+    // 2. ativar a função de componente InvestmentCard
+    const newInvestmentCard = InvestmentCard(investment);
+
+    // 3. inserir o componente criado
+    gridInvestments.insertAdjacentHTML('afterbegin', newInvestmentCard);
+}
+
+window.tratarEvento = () => alert('clique realizado no h1');
+
+addInvestment(investments[0]);
+
+addInvestment(investments[1]);
